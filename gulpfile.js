dc2f61c1a3d77660a52a246304ba8d486de50f3c
xychenweb <xychenweb@gmail.com>
2017-09-18 03:23:01 -0700

    menu bar desktop complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,34 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
     livereload = require('gulp-livereload');
 
 //style paths
 var sassFiles = 'sass/**/*.scss',
-    cssDest = 'css/';
+    cssDest = 'css/',
+    htmlFile = 'index.html';
 
-gulp.task('webserver', function() {
-  connect.server({
-  	livereload: true
-  });
+gulp.task('html', function() {
+     gulp.src(htmlFile)
+        .pipe(livereload());
 });
-
 gulp.task('styles', function() {
     gulp.src(sassFiles)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(cssDest))
         .pipe(livereload());
 });
-
+gulp.task('webserver', function() {
+    connect.server({
+        livereload: true
+    });
+});
 gulp.task('watch', function() {
-	livereload.listen();
+    livereload.listen();
     gulp.watch(sassFiles, ['styles']);
+    gulp.watch(htmlFile, ['html']);
 });
 
-gulp.task('default', ['styles','webserver','watch']);
+gulp.task('default', ['html','styles', 'webserver', 'watch']);
